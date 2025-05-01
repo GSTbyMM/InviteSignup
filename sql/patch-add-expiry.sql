5c366dcd5ad92a3cf4516ad9a022adac22f2288e
GSTbyMM <136344864+GSTbyMM@users.noreply.github.com>
2025-05-01 11:03:21 +0530

    Update patch-add-expiry.sql

--- sql/patch-add-expiry.sql
+++ sql/patch-add-expiry.sql
@@ -1,1 +1,3 @@
 ALTER TABLE invitesignup ADD COLUMN expiry varbinary(32) DEFAULT NULL;
+ALTER TABLE invitesignup MODIFY is_invitee int(10) unsigned NULL;
+ALTER TABLE invitesignup MODIFY is_used binary(14) NULL;
