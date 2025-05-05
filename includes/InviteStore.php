a19718136062da3b1a3d9be5da7e227aaca71a3f
GSTbyMM <admin@gstbymm.com>
2025-05-05 22:34:53 +0530

    renewel v0.0.4 - Expiry updated at InviteSignup table as well

--- includes/InviteStore.php
+++ includes/InviteStore.php
@@ -24,6 +24,12 @@ class InviteStore {
 		$this->dbTable = $table;
 	}
 	
+	public function updateExpiryByEmail($email, $newExpiry) {
+		$conds = [ 'is_email' => $email, 'is_invitee IS NOT NULL' ];
+		$data = [ 'expiry' => $newExpiry ];
+		$this->db->update($this->dbTable, $data, $conds, __METHOD__);
+	}
+	
 	public function getTableName() {
         return $this->dbTable;
     }
